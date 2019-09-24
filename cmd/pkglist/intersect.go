1b7f561ad9b5040be1864f626e71c396fe80aa91
Daniele Rondina <geaaru@sabayonlinux.org>
2019-09-25 01:13:15 +0200

    Update copyright

--- cmd/pkglist/intersect.go
+++ cmd/pkglist/intersect.go
@@ -1,6 +1,6 @@
 /*
 
-Copyright (C) 2017-2018  Daniele Rondina <geaaru@sabayonlinux.org>
+Copyright (C) 2017-2019  Daniele Rondina <geaaru@sabayonlinux.org>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
