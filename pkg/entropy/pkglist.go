1f444644b1629de64118e10632984e42bc1311f2
Daniele Rondina <geaaru@sabayonlinux.org>
2019-09-26 10:17:05 +0200

    Add EntropyPackage definition

---
+++ pkg/entropy/pkglist.go
@@ -0,0 +1,91 @@
+/*
+
+Copyright (C) 2017-2019  Daniele Rondina <geaaru@sabayonlinux.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+package entropy
+
+import (
+	"errors"
+	"regexp"
+	"strconv"
+
+	gentoo "github.com/Sabayon/pkgs-checker/pkg/gentoo"
+)
+
+type EntropyPackage struct {
+	*gentoo.GentooPackage
+	Revision int
+}
+
+func EntropyIsPkgWithRevision(pkgname string) (ans bool) {
+	ans = false
+
+	if pkgname != "" {
+		regexRev := regexp.MustCompile(
+			"[~][0-9]*$",
+		)
+
+		matches := regexRev.FindAllString(pkgname, -1)
+		if len(matches) > 0 {
+			ans = true
+		}
+	}
+
+	return
+}
+
+func NewEntropyPackage(pkgname string) (*EntropyPackage, error) {
+	var ans *EntropyPackage
+
+	if pkgname == "" {
+		return nil, errors.New("Invalid pkgname")
+	}
+
+	regexRev := regexp.MustCompile(
+		"[~][0-9]*$",
+	)
+
+	matches := regexRev.FindAllString(pkgname, -1)
+	if len(matches) > 0 {
+		gPkgname := pkgname[:len(pkgname)-len(matches[0])]
+
+		gp, err := gentoo.ParsePackageStr(gPkgname)
+		if err != nil {
+			return nil, err
+		}
+
+		rev, err := strconv.Atoi(matches[0][1:])
+		if err != nil {
+			return nil, err
+		}
+		ans = &EntropyPackage{
+			GentooPackage: gp,
+			Revision:      rev,
+		}
+	} else {
+		gp, err := gentoo.ParsePackageStr(pkgname)
+		if err != nil {
+			return nil, err
+		}
+		ans = &EntropyPackage{
+			GentooPackage: gp,
+			Revision:      0,
+		}
+	}
+
+	return ans, nil
+}
---
+++ pkg/entropy/pkglist_test.go
@@ -0,0 +1,57 @@
+/*
+
+Copyright (C) 2017-2019  Daniele Rondina <geaaru@sabayonlinux.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+package entropy_test
+
+import (
+	. "github.com/Sabayon/pkgs-checker/pkg/entropy"
+	. "github.com/Sabayon/pkgs-checker/pkg/gentoo"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Entropy Pkglist", func() {
+
+	Describe("Parse String1", func() {
+
+		ep, err := NewEntropyPackage("sys-fs/udftools-2.1~1")
+
+		Context("Check processing phase", func() {
+			It("Check error", func() {
+				Expect(err).Should(BeNil())
+			})
+
+			It("Check element", func() {
+				Expect(ep).Should(Equal(&EntropyPackage{
+					GentooPackage: &GentooPackage{
+						Category:  "sys-fs",
+						Name:      "udftools",
+						Version:   "2.1",
+						Slot:      "0",
+						Condition: PkgCondEqual,
+					},
+					Revision: 1,
+				}))
+			})
+
+		})
+
+	})
+
+})
