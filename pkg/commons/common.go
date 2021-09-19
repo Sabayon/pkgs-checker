586b6f48418a5169b483a5c3722faf2a0645f91d
Daniele Rondina <geaaru@sabayonlinux.org>
2021-09-19 15:57:45 +0200

    Prepare release v0.9.0

--- pkg/commons/common.go
+++ pkg/commons/common.go
@@ -30,7 +30,7 @@ import (
 	settings "github.com/spf13/viper"
 )
 
-const PKGS_CHECKER_VERSION = "0.8.4"
+const PKGS_CHECKER_VERSION = "0.9.0"
 const PKGS_CHECKER_EMPTY_PKGHASH = "00000000000000000000000000000000"
 
 func InitConcurrency() {
